f13d6d301d5cec831c586d3646333db0567aa902
ammar <ammar.botonjic@convergent-usa.com>
2020-09-26 16:45:45 +0200

    Creating endpoint

--- backend/src/server/app.js
+++ backend/src/server/app.js
@@ -1,13 +1,39 @@
 const express = require('express');
+const cities = require('../static/json/cities.json');
+
 
 const app = express();
+
+const findCities = (cityName) => {
+    // Counter for 5 cities
+    let count = 0;
+
+    // Convert first letter to uppercase
+    const modifiedCityName = cityName[0].toUpperCase() + cityName.substring(1);
+
+    // All matches should be saved inside citiesArray
+    let citiesArray = []
+
+    for (let index = 0; index < cities.length; index++) {
+        if (cities[index].name.startsWith(modifiedCityName)) {
+            count += 1;
+            if (count === 5) {
+                break;
+            }
+            citiesArray.push(cities[index].name);
+        }
+    }
+    return citiesArray;
+}
+
+findCities('new')
 
 app.get('', (req, res) => {
     res.send('Hello express!');
 });
 
-app.get('/cities', (req, res) => {
-    res.send('Cities page!');
+app.get(`/cities/:cityName`, (req, res) => {
+    res.send(findCities(req.params.cityName));
 })
 
 app.listen(8080, () => {
