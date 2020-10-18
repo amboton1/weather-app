03b3177b421f399f486da443077c54b2305268d1
ammar <ammar.botonjic@convergent-usa.com>
2020-10-18 16:21:45 +0200

    Writing logic for two endpoints, frontend part

--- backend/src/server/app.js
+++ backend/src/server/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const fs = require('fs');
+const readline = require('readline');
 const { findCities } = require('../controllers/cityService');
 
 const app = express();
+app.use(express.json());
 
 app.get('', (req, res) => {
     res.send('Hello express!');
@@ -9,6 +12,53 @@ app.get('', (req, res) => {
 
 app.get(`/cities/:cityName`, (req, res) => {
     res.send(findCities(req.params.cityName));
+});
+
+app.post('/user-cities', (req, res) => {
+    try {
+        if (fs.existsSync(`${req.body.user}.txt`)) {
+            let counter = 0;
+            let onlyUserCities = [];
+            const userFileData = fs.readFileSync(`${req.body.user}.txt`, 'utf8');
+
+            if (userFileData.includes(`${req.body.token}`)) {
+                userFileData.split(/\n/).forEach((line) => {
+                    if (counter !== 0) {
+                        onlyUserCities.push(line);
+                    }
+                    counter += 1;
+                });
+                res.json(onlyUserCities);
+            } else {
+                res.send(403);
+            }
+        } else {
+            res.send(404);
+        }
+    } catch(err) {
+        console.error(err);
+    }
+});
+
+app.post('/user-cities/new-city', (req, res) => {
+    try {
+        if (fs.existsSync(`${req.body.user}.txt`)) {
+            const data = fs.readFileSync(`${req.body.user}.txt`, 'utf8');
+            if (data.includes(`${req.body.city}`)) {
+                return;
+            } else {
+                fs.appendFile(`${req.body.user}.txt`, `\n${req.body.city}`, (err) => {
+                    if (err) throw err;
+                })
+            }
+        } else {
+            fs.appendFile(`${req.body.user}.txt`, `${req.body.token}\n${req.body.city}`, (err) => {
+                if (err) throw err;
+            })
+        }
+    } catch(err) {
+        console.error(err)
+    }
 });
 
 app.get('*', (req, res) => {
